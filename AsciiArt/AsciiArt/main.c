3b09fae8a06063dfa28575337999ce70db6eb991
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2014-12-02 20:13:58 +0100

    Added Xcode project

---
+++ AsciiArt/AsciiArt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AsciiArt
+//
+//  Created by Alessandro Fusco on 02/12/14.
+//  Copyright (c) 2014 Alessandro Fusco. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
